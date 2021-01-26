6bb3325a1ba1276ccedff78281c658b6cc02c414
gyun <independenceday888@gmail.com>
2021-01-27 00:34:08 +0900

    파일하나올림

---
+++ python/module_practice/check.py
@@ -0,0 +1,6 @@
+# check.py
+def odd(n):
+    return bool(n % 2)
+
+def even(n):
+    return not bool(n % 2)
---
+++ python/module_practice/my_algo.py
@@ -0,0 +1,20 @@
+# import check
+
+# print(check.odd(3))
+
+
+# print(dir(check))
+
+
+# from my_package.math import tools
+# from my_package.math.tools import pi
+from my_package.math.tools import *
+
+# print(dir(tools))
+# print(pi)
+
+
+print(my_max(3,4,))
+
+
+
